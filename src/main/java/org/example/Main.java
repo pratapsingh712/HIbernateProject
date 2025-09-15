2cb19a9fc873ea551be5de82b90067fe756d30a8
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-09-15 11:45:56 +0530

    Added the merge method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,11 @@ public class Main {
     public static void main(String[] args) {
 
         Student s = new Student();
-        s.setRoll_No(7);
-        s.setS_Name("Ram");
+        s.setRoll_No(6);
+        s.setS_Name("Parvati");
         s.setS_Age(18);
 
-        Student s1 = new Student();
+//        Student s1 = new Student();
 
 //        Configuration cfg = new Configuration();
 //        cfg.addAnnotatedClass(org.example.Student.class);
@@ -28,17 +28,19 @@ public class Main {
 
         Session session = sf.openSession();
 
-        //Transaction transaction = session.beginTransaction(); we need transaction when we are performing insert, update or delete not when fetching the data
+        Transaction transaction = session.beginTransaction(); //we need transaction when we are performing insert, update or delete not when fetching the data
 
-        s1 = session.find(Student.class,1);
+//        s1 = session.find(Student.class,1);
 
         //session.persist(s); // instead of this earlier session.save(object); was used but to match the JPA standard naming convention it was changed to persist
 
-        //transaction.commit();
+        session.merge(s);
+        transaction.commit();
+
         session.close();
         sf.close();
 
-        System.out.println(s1); // if you want to perform something like s1.getName or s1.getAge then it's good idea to use try catch because it can throw null pointer exception
+        System.out.println(s); // if you want to perform something like s1.getName or s1.getAge then it's good idea to use try catch because it can throw null pointer exception
 
     }
 }
